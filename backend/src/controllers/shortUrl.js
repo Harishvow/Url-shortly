c331f750bf3b9ef6cec22a82c08b48d865fdb40e
harishvow <harisharish982005@gmail.com>
2026-01-16 18:41:29 +0530

    controller redirect

--- backend/src/controllers/shortUrl.js
+++ backend/src/controllers/shortUrl.js
@@ -1,25 +1,41 @@
-import {nanoid } from "nanoid";  
+import { nanoid } from "nanoid";
 import URL from "../model/urlbackend.js";
 
 export async function GenerateShortUrl(req, res) {
-  const body = req.body;
-  if (!body.url) return res.status(400).json({ error: "URL is required" });
+  let originalUrl = req.body.url;
+
+  if (!originalUrl) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+  if (
+    !originalUrl.startsWith("http://") &&
+    !originalUrl.startsWith("https://")
+  ) {
+    originalUrl = "https://" + originalUrl;
+  }
+
   const shortID = nanoid(8);
+
   await URL.create({
     shortId: shortID,
-    redirectURL: req.body.url,
+    redirectURL: originalUrl,
     visitHistory: [],
   });
-  const Shorturl= `${process.env.BASE_URL}/${shortID}`;
+
+  const Shorturl = `${process.env.BASE_URL}/${shortID}`;
   return res.json({ Shorturl });
 }
-export async function findURL(req,res){ 
-  const shortId=req.params.shortId;
-  const result=await URL.findOne({shortId});
-  console.log(`totalClicks:${result.visitHistory.length}`);
+
+export async function findURL(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+
+  if (!result) {
+    return res.status(404).json({ error: "Short URL not found" });
+  }
+
   return res.json({
     totalClicks: result.visitHistory.length,
     analytics: result.visitHistory,
   });
 }
-
